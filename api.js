71b26b1aede9f2a5fb9001c5be1a52c6a1ec7195
kuviro <113908632+kuviro@users.noreply.github.com>
2022-09-19 20:21:48 +0700

    Update api.js

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 //Url to Google Sheets
 //const url = 'https://docs.google.com/spreadsheets/d/[sheet ID]/gviz/tq?';
-const url = 'https://docs.google.com/spreadsheets/d/1qqcewCVPu3hTQTlgLBHbPWKJShlVnEiKu4ZdHLctPvc/gviz/tq?';
+const url = 'https://docs.google.com/spreadsheets/d/1ShBZMESIj-VX-oV-aoDytHD0wBsD-l2mg9T2ByIw6x4/gviz/tq?';
 const productsContainer = document.querySelector('.products-grid');
     fetch(url)
     .then(res => res.text())
